d8a9d9c7cecb55f043fef4a5a6d17a2c82e117a4
Totomomokazu <kanketomokazu@gmail.com>
2023-12-27 22:59:28 +0900

    アカウント画面の作成

---
+++ account_create.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アカウント登録</title>
+</head>
+<body>
+    <!-- <button><a href="select.php">データベースを確認する</a></button> -->
+    <form method="post" action="★★★★★.php">
+        <fieldset>
+            <legend>アカウント作成</legend>
+            <label>氏名：<input type="text" name="user_name"></label><br>
+            <label>ID：<input type="text" name="user_id"></label><br>
+            <label>PW：<input type="text" name="pw"></label><br>
+            <input type="submit" value="登録">
+        </fieldset>
+    </form>    
+</body>
+</html>
